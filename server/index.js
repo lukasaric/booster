d7038c956bb74aa5b20e04e317ead37b73c929dc
Luka Saric <rpg.ari7@gmail.com>
2021-03-03 12:27:48 +0100

    :wrench:

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
+const { ip, port } = require('./config');
 const app = require('./app');
 const database = require('./common/database');
-const { port } = require('./common/origin');
 const Promise = require('bluebird');
 const { promisify } = require('util');
 const logger = require('./common/logger')();
@@ -13,7 +13,7 @@ Promise.config({ longStackTraces: !isProduction });
 const runServer = promisify(app.listen.bind(app));
 
 database.initialize()
-  .then(() => runServer(port))
+  .then(() => runServer(port, ip))
   .then(() => logger.info(`Server listening on port ${port}`))
   .catch(err => {
     logger.fatal(err, 'Starting server failed');
