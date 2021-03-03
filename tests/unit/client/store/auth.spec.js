ccf49e5898c1e193d3b11eb15c432e36ff5b876b
Luka Saric <rpg.ari7@gmail.com>
2021-03-03 10:22:15 +0100

    Tests formatting cleanup :nail_care:

--- tests/unit/client/store/auth.spec.js
+++ tests/unit/client/store/auth.spec.js
@@ -1,13 +1,13 @@
 const { login } = require('@/store/modules/auth/actions');
 
 jest.mock('@/api/auth', () => {
-  const responseUser = { id: 1, email: 'user@gmail.com', password: 'password' }
+  const responseUser = { id: 1, email: 'user@gmail.com', password: 'password' };
   return { login: jest.fn().mockResolvedValue(responseUser) };
 });
 
 describe('Test for user login', () => {
   it('Should login successfully', async () => {
-    const mockedUser = { email: 'user@gmail.com', password: 'password' }
+    const mockedUser = { email: 'user@gmail.com', password: 'password' };
     const commit = jest.fn();
     await login({ commit }, mockedUser);
     expect(commit).toHaveBeenCalledWith('login', { id: 1, ...mockedUser });
--- tests/unit/client/utils/vehicle.spec.js
+++ tests/unit/client/utils/vehicle.spec.js
@@ -1,15 +1,15 @@
 const vehicleApi = require('@/api/vehicle').default;
 
 jest.mock('@/api/request', () => {
-  const item = { id: 1, make: 'AUDI', model: 'A3', make: 2010 };
+  const item = { id: 1, make: 'AUDI', model: 'A3', year: 2010 };
   const items = [
     item,
-    { id: 2, make: 'ACURA', model: 'X1', make: 2000 },
-    { id: 3, make: 'HONDA', model: 'X2', make: 2005 }
+    { id: 2, make: 'ACURA', model: 'X1', year: 2000 },
+    { id: 3, make: 'HONDA', model: 'X2', year: 2005 }
   ];
   return {
-    get: jest.fn().mockResolvedValue({ data: { data: { items }} }),
-    post: jest.fn().mockResolvedValue({ data: { data: { item }} })
+    get: jest.fn().mockResolvedValue({ data: { data: { items } } }),
+    post: jest.fn().mockResolvedValue({ data: { data: { item } } })
   };
 });
 
@@ -21,7 +21,7 @@ describe('Test Vehicle API', () => {
   });
 
   it('should create vehicle', async () => {
-    const payload = { make: 'AUDI', model: 'A3', make: 2010 };
+    const payload = { make: 'AUDI', model: 'A3', year: 2010 };
     const vehicle = await vehicleApi.create(payload);
     expect(vehicle).toBeTruthy();
   });
